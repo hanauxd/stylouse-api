dfb3336a9d6e2106ac6f138ed43da59682bd108b
Fathimath Hanau <hanau.xd@gmail.com>
2020-01-11 07:24:25 +0530

    Override Spring Security and add JWT token.

---
+++ src/main/java/lk/apiit/eea/stylouse/security/JwtRequestFilter.java
@@ -0,0 +1,58 @@
+package lk.apiit.eea.stylouse.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Value("${app.token.prefix}")
+    private String TOKEN_PREFIX;
+    @Value("${app.token.header}")
+    private String TOKEN_HEADER;
+    private JwtUtil jwtUtil;
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public JwtRequestFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = httpServletRequest.getHeader(TOKEN_HEADER);
+        String username = null;
+        String jwt = null;
+
+        try {
+            if (authHeader != null && authHeader.startsWith(TOKEN_PREFIX)) {
+                jwt = authHeader.substring(7);
+                username = jwtUtil.extractUsername(jwt);
+            }
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                if (jwtUtil.validateToken(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    token.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                    SecurityContextHolder.getContext().setAuthentication(token);
+                }
+            }
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
