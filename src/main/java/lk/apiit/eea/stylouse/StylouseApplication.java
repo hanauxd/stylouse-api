871725eea00d5a23ae2c8d8abb356d926ff78365
Fathimath Hanau <hanau.xd@gmail.com>
2020-01-11 05:46:26 +0530

    Initial commit

---
+++ src/main/java/lk/apiit/eea/stylouse/StylouseApplication.java
@@ -0,0 +1,13 @@
+package lk.apiit.eea.stylouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StylouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/apiit/eea/stylouse/StylouseApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.apiit.eea.stylouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
