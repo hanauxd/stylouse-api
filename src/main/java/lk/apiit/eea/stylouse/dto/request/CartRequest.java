772abf6091f46effaa2908caad75019a4f33d78f
Fathimath Hanau <hanau.xd@gmail.com>
2020-01-26 21:48:14 +0530

    Request objects

---
+++ src/main/java/lk/apiit/eea/stylouse/dto/request/CartRequest.java
@@ -0,0 +1,13 @@
+package lk.apiit.eea.stylouse.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartRequest {
+    private String productId;
+    private int quantity;
+}
