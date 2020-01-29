c68ed9d947b562a7fde71d9bbad3a093bf2359b8
Fathimath Hanau <hanau.xd@gmail.com>
2020-01-29 20:58:34 +0530

    OrderController handles getting order history for logged in user and get order by id.

---
+++ src/main/java/lk/apiit/eea/stylouse/controllers/OrdersController.java
@@ -0,0 +1,44 @@
+package lk.apiit.eea.stylouse.controllers;
+
+import lk.apiit.eea.stylouse.models.Orders;
+import lk.apiit.eea.stylouse.models.User;
+import lk.apiit.eea.stylouse.services.OrdersService;
+import lk.apiit.eea.stylouse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrdersController {
+    private OrdersService ordersService;
+    private UserService userService;
+
+    @Autowired
+    public OrdersController(OrdersService ordersService, UserService userService) {
+        this.ordersService = ordersService;
+        this.userService = userService;
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping
+    public ResponseEntity<?> getUserOrders(Authentication auth) {
+        User user = userService.getUserByEmail(auth.getName());
+        List<Orders> orders = ordersService.getUserOrders(user);
+        return ResponseEntity.ok(orders);
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOrderById(@PathVariable String id) {
+        Orders order = ordersService.getOrderById(id);
+        return ResponseEntity.ok(order);
+    }
+}
