9daa0e4b5061ca01f8de7c5913611f31cb6dd4ff
Fathimath Hanau <hanau.xd@gmail.com>
2020-01-29 21:15:37 +0530

    Orders service

---
+++ src/main/java/lk/apiit/eea/stylouse/services/OrdersService.java
@@ -0,0 +1,31 @@
+package lk.apiit.eea.stylouse.services;
+
+import lk.apiit.eea.stylouse.exceptions.CustomException;
+import lk.apiit.eea.stylouse.models.Orders;
+import lk.apiit.eea.stylouse.models.User;
+import lk.apiit.eea.stylouse.repositories.OrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrdersService {
+    private OrdersRepository ordersRepository;
+
+    @Autowired
+    public OrdersService(OrdersRepository ordersRepository) {
+        this.ordersRepository = ordersRepository;
+    }
+
+    public List<Orders> getUserOrders(User user) {
+        return ordersRepository.findByUser(user);
+    }
+
+    public Orders getOrderById(String id) {
+        return ordersRepository.findById(id).orElseThrow(
+                () -> new CustomException("Order not found", HttpStatus.NOT_FOUND)
+        );
+    }
+}
