ec6cdbb61fe22301172a05a3be0edd97eb04afab
Fathimath Hanau <hanau.xd@gmail.com>
2020-01-30 03:30:23 +0530

    Address entity is removed.

--- src/main/java/lk/apiit/eea/stylouse/models/User.java
+++ src/main/java/lk/apiit/eea/stylouse/models/User.java
@@ -31,16 +31,8 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Cart> carts = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
-    private List<Address> addresses = new ArrayList<>();
-
     public void removeCart(Cart cart) {
         cart.setUser(null);
         this.carts.remove(cart);
-    }
-
-    public void removeAddress(Address address) {
-        address.setUser(null);
-        this.addresses.remove(address);
     }
 }
