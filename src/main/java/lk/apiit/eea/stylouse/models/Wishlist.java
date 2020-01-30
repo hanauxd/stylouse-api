f380895defef21dc01816b17104eb1521f2243a3
Fathimath Hanau <hanau.xd@gmail.com>
2020-01-30 05:33:50 +0530

    Wishlist request and entity

---
+++ src/main/java/lk/apiit/eea/stylouse/models/Wishlist.java
@@ -0,0 +1,36 @@
+package lk.apiit.eea.stylouse.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Wishlist {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(length = 36)
+    private String id;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user", referencedColumnName = "id")
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "product", referencedColumnName = "id")
+    @JsonIgnore
+    private Product product;
+
+    public Wishlist(User user, Product product) {
+        this.user = user;
+        this.product = product;
+    }
+}
