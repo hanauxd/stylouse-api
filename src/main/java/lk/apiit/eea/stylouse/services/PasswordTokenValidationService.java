a9b0c4a97715b8f6bf73c5b4037ef7f6cb179bdd
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-19 11:59:50 +0530

    Password token validation

---
+++ src/main/java/lk/apiit/eea/stylouse/services/PasswordTokenValidationService.java
@@ -0,0 +1,31 @@
+package lk.apiit.eea.stylouse.services;
+
+import lk.apiit.eea.stylouse.exceptions.CustomException;
+import lk.apiit.eea.stylouse.models.ResetPasswordToken;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class PasswordTokenValidationService {
+
+    public void validateToken(ResetPasswordToken persistedToken, String token) {
+        if (persistedToken == null) {
+            throw new CustomException("Token not found.", HttpStatus.NOT_FOUND);
+        }
+        if (isTokenExpired(persistedToken)) {
+            throw new CustomException("Token is expired.", HttpStatus.BAD_REQUEST);
+        }
+        if (!isTokenMatched(persistedToken.getToken(), token)) {
+            throw new CustomException("Invalid token.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private boolean isTokenExpired(ResetPasswordToken token) {
+        return token.getExpiryDate().before(new Date());
+    }
+
+    private boolean isTokenMatched(String persistedToken, String token) {
+        return persistedToken.equals(token);
+    }}
