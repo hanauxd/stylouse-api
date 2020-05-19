933c08dee648aef824c5e1aa509ec96d8837d637
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-19 11:58:12 +0530

    Repo method - find token by User

--- src/main/java/lk/apiit/eea/stylouse/repositories/ResetPasswordTokenRepository.java
+++ src/main/java/lk/apiit/eea/stylouse/repositories/ResetPasswordTokenRepository.java
@@ -1,9 +1,11 @@
 package lk.apiit.eea.stylouse.repositories;
 
 import lk.apiit.eea.stylouse.models.ResetPasswordToken;
+import lk.apiit.eea.stylouse.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResetPasswordTokenRepository extends JpaRepository<ResetPasswordToken, String> {
+    ResetPasswordToken findByUser(User user);
 }
